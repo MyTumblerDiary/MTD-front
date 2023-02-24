8c46e6703959bf6486a9a5e4f723773dcd4823cc
task11 <task11@naver.com>
2023-02-24 15:22:09 +0900

    feat: add temp atom

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,11 +3,14 @@ import "@/styles/globals.css";
 
 import { ApolloProvider } from "@apollo/client";
 import { client } from "@/apollo/client";
+import { RecoilRoot } from "recoil";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ApolloProvider client={client}>
-      <Component {...pageProps} />
-    </ApolloProvider>
+    <RecoilRoot>
+      <ApolloProvider client={client}>
+        <Component {...pageProps} />
+      </ApolloProvider>
+    </RecoilRoot>
   );
 }
