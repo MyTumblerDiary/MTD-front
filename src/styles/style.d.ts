a163ab32dad43c6c53bcf358745550a7740c2c05
uuuuujin <yearn8546@gmail.com>
2023-03-14 22:40:23 +0900

    feat: theme파일에 컬러 정리

---
+++ src/styles/style.d.ts
@@ -0,0 +1,25 @@
+import 'styled-components';
+
+declare module 'styled-components' {
+  interface DefaultTheme {
+    color: {
+      main: string;
+      g900: string;
+      g800: string;
+      g700: string;
+      g600: string;
+      g500: string;
+      g400: string;
+      g300: string;
+      g200: string;
+      g100: string;
+      success: string;
+      notice: string;
+      warning: string;
+      error: string;
+      info: string;
+    };
+
+    maxWidth: string;
+  }
+}
---
+++ src/styles/theme.ts
@@ -0,0 +1,25 @@
+import { DefaultTheme } from 'styled-components';
+
+const theme: DefaultTheme = {
+  color: {
+    main: '#A435EF',
+    g900: '#222222',
+    g800: '#4D4D4D',
+    g700: '#656565',
+    g600: '#707070',
+    g500: '#8E8E8E',
+    g400: '#BCBCBC',
+    g300: '#E4E4E4',
+    g200: '#EFEFEF',
+    g100: '#F5F5F5',
+    success: '#09E36B',
+    notice: '#FFD83B',
+    warning: '#FF8B48',
+    error: '#FF4E44',
+    info: '#6570D6'
+  },
+
+  maxWidth: '768px'
+};
+
+export default theme;
