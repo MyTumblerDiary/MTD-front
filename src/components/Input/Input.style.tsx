64946a420e1d2e74c37132e7f8398ede05e8c6ac
task11 <task11@naver.com>
2023-03-15 19:05:09 +0900

    refactor: add input props

--- src/components/Input/Input.style.tsx
+++ src/components/Input/Input.style.tsx
@@ -4,10 +4,14 @@ interface Props {
   sizeStyle: CSSProp;
 }
 
+export const Label = styled.label`
+  display: none;
+`;
+
 export const Input = styled.input<Props>`
   ${(props) => props.sizeStyle};
   width: var(--input--width, 280px);
   height: var(--input--height, 48px);
-  border: 1px solid #8e8e8e;
+  border: 1px solid #e4e4e4;
   border-radius: 8px;
 `;
--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -3,11 +3,27 @@ import * as Style from './Input.style';
 import { SIZES } from './Input.variant';
 
 interface Props {
+  type: 'text' | 'password' | 'email';
+  name: string;
   size: 'sm' | 'md';
+  value: string | number;
+  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  placeholder?: string;
 }
 
-export default function Input({ size }: Props) {
+export default function Input({ name, size, onChange, ...rest }: Props) {
   const sizeStyle = SIZES[size];
 
-  return <Style.Input sizeStyle={sizeStyle} />;
+  return (
+    <>
+      <Style.Label htmlFor={name}>{name}</Style.Label>
+      <Style.Input
+        id={name}
+        name={name}
+        sizeStyle={sizeStyle}
+        onChange={onChange}
+        {...rest}
+      />
+    </>
+  );
 }
