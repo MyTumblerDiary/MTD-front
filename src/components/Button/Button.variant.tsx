93afbeab87da6957b1de99f1272afe1d2e6c371d
task11 <task11@naver.com>
2023-03-16 16:59:56 +0900

    fix: typo fix

--- src/components/Button/Button.variant.tsx
+++ src/components/Button/Button.variant.tsx
@@ -2,11 +2,11 @@ import { css } from 'styled-components';
 
 export const SIZES = {
   sm: css`
-    --button--width: 120px;
-    --button--height: 48px;
+    --button-width: 120px;
+    --button-height: 48px;
   `,
   md: css`
-    --button--width: 280px;
-    --button--height: 48px;
+    --button-width: 280px;
+    --button-height: 48px;
   `
 };
