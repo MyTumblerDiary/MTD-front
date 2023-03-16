86ceb5f723721107d57c83ed6c2126e72a2c822d
task11 <task11@naver.com>
2023-03-16 16:33:25 +0900

    feat: add search, bracket down icons

---
+++ src/components/Icons/BracketDown.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+export default function BracketDown() {
+  return (
+    <svg
+      width='28'
+      height='28'
+      viewBox='0 0 28 28'
+      fill='none'
+      xmlns='http://www.w3.org/2000/svg'
+    >
+      <g clip-path='url(#clip0_107_2185)'>
+        <path
+          d='M26.46 8.40009L25.2919 7.23193L14 18.5238L2.7082 7.23193L1.54004 8.40009L14 20.8601L26.46 8.40009Z'
+          fill='black'
+        />
+      </g>
+      <defs>
+        <clipPath id='clip0_107_2185'>
+          <rect width='28' height='28' fill='white' />
+        </clipPath>
+      </defs>
+    </svg>
+  );
+}
