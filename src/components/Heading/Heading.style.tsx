e09b09ac56bd1d69c543c4f2ff4359ced3059f41
task11 <task11@naver.com>
2023-03-18 12:45:14 +0900

    refactor: refactor tag to semantically

--- src/components/Heading/Heading.style.tsx
+++ src/components/Heading/Heading.style.tsx
@@ -1,16 +1,16 @@
 import styled from 'styled-components';
 
-export const HeadTitle = styled.span`
+export const HeadTitle = styled.h1`
   font-size: 36px;
   font-weight: 700;
 `;
 
-export const SubTitle = styled.span`
+export const SubTitle = styled.h2`
   font-size: 24px;
   font-weight: bold;
 `;
 
-export const Title = styled.span`
+export const Title = styled.h3`
   font-size: 18px;
   font-weight: bold;
 `;
