ca8de0fbd71b51fd64205b8bc82b9aba54f8b976
uuuuujin <yearn8546@gmail.com>
2023-03-18 17:44:05 +0900

    feat: svg 파일 추가

---
+++ src/components/svg.tsx
@@ -0,0 +1,9 @@
+import Home from '../../public/icons/Home.svg';
+import Cafe from '../../public/icons/Cafe.svg';
+import Mypage from '../../public/icons/Mypage.svg';
+
+export default {
+  Home,
+  Cafe,
+  Mypage
+};
