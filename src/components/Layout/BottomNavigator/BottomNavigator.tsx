64411a42446e25a011657fcc87a7bb7f965506f6
uuuuujin <yearn8546@gmail.com>
2023-03-22 21:16:41 +0900

    refactor: navigator 상수화

--- src/components/Layout/BottomNavigator/BottomNavigator.tsx
+++ src/components/Layout/BottomNavigator/BottomNavigator.tsx
@@ -2,35 +2,17 @@ import Link from 'next/link';
 
 import * as Style from './BottomNavigator.style';
 
-import Svg from '../../svg';
+import NAVIGATOR from '@/constants/navigator';
 
 export default function BottomNavigator() {
-  const NAVIGATORS = [
-    {
-      icon: <Svg.Cafe />,
-      title: '카페',
-      url: '/cafes'
-    },
-    {
-      icon: <Svg.Home />,
-      title: '홈',
-      url: '/'
-    },
-    {
-      icon: <Svg.Mypage />,
-      title: '마이페이지',
-      url: '/mypage'
-    }
-  ];
-
   return (
     <Style.NavigatorContainer>
       <Style.NavigatorBox>
-        {NAVIGATORS.map((navigator) => {
-          const { icon, title, url } = navigator;
+        {Object.values(NAVIGATOR).map((navigator) => {
+          const { icon, title, path } = navigator;
           return (
             <Style.ElementContainer key={`navigator ${title}`}>
-              <Link href={url}>
+              <Link href={path}>
                 <div>{icon}</div>
                 <Style.IconTitle>{title}</Style.IconTitle>
               </Link>
