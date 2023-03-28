cd1e59ce54a6dabc4f54ff7682c619588379d298
task11 <task11@naver.com>
2023-03-28 23:26:10 +0900

    refactor: input ui 수정

--- src/components/Common/Input/Input.style.tsx
+++ src/components/Common/Input/Input.style.tsx
@@ -9,13 +9,22 @@ interface InputProps {
 }
 
 export const InputWithLabel = styled.div`
+  position: relative;
   display: flex;
   flex-direction: column;
   gap: 6px;
 `;
 
 export const Label = styled.label`
+  position: absolute;
+  left: 8px;
+  top: -10px;
+  z-index: 99;
+  background-color: #ffffff;
+  padding: 5px;
   display: flex;
+  align-items: center;
+  gap: 2px;
 `;
 
 export const InputWrapper = styled.div<InputWrapperProps>`
@@ -30,7 +39,8 @@ export const Input = styled.input<InputProps>`
   ${(props) => props.variantStyle};
   width: 100%;
   height: 100%;
-  border: 1px solid var(--input-border, #d1d1d1);
+  padding: 15px 18px;
+  border: 1px solid var(--input-border, #f5f5f5);
   border-radius: 8px;
 
   :disabled {
