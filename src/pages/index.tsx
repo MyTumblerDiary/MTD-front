dee6eba465c2330e4dc09f393e83e2f188886fe3
uuuuujin <yearn8546@gmail.com>
2023-03-29 21:42:17 +0900

    feat: head 컴포넌트 구현

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,12 @@
+import PageHead from '@/components/Head/Head';
+
 export default function Home() {
   return (
     <>
-      <div>Hi</div>
+      <div>
+        <PageHead title='홈' />
+        Hi
+      </div>
     </>
   );
 }
