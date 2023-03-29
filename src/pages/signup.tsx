6e37835c915ff3e23512251589e64fa5c5c9f4e6
task11 <task11@naver.com>
2023-03-29 19:48:06 +0900

    feat: 기존 회원가입 페이지 제거

--- src/pages/signup.tsx
+++
@@ -1,9 +0,0 @@
-import Layout from '../components/Layout/Layout';
-
-export default function Signup() {
-  return <div>회원가입</div>;
-}
-
-Signup.getLayout = function getLayout(page: React.ReactElement) {
-  return <Layout bottomNavigator={false}>{page}</Layout>;
-};
