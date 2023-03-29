65f33c592b1889f973c074ec94edb0da98478613
uuuuujin <yearn8546@gmail.com>
2023-03-29 12:39:18 +0900

    feat: typography 타입 추가

--- src/types/typography.type.ts
+++ src/types/typography.type.ts
@@ -1,5 +1,5 @@
 export interface TypographyProps {
-  size: 'body1' | 'body2' | 'body3' | 'button1' | 'button2';
-  variant?: 'default' | 'accent' | 'gray';
+  size: 'body1' | 'body2' | 'body3' | 'caption' | 'button1' | 'button2';
+  variant?: 'default' | 'accent' | 'gray' | 'success' | 'error';
   children: React.ReactNode;
 }
