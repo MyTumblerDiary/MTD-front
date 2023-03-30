97532d4d0938b5e32b2ebdf0f0fada6573dfffb2
task11 <task11@naver.com>
2023-03-30 18:56:29 +0900

    feat: signup 페이지 폼 UI 구현

---
+++ src/components/Signup/SignupForm.style.ts
@@ -0,0 +1,126 @@
+import styled, { keyframes } from 'styled-components';
+
+export const SignupWrapper = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+`;
+
+export const SignupHeader = styled.div`
+  width: 100%;
+  flex: 1;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const SignupForm = styled.form`
+  width: 100%;
+  flex: 4;
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+export const InputWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 20px;
+`;
+
+export const SignupButtonWrapper = styled.div`
+  margin-bottom: 12%;
+  animation-name: Down;
+  animation-duration: 0.6s;
+  animation-timing-function: ease-in;
+
+  @keyframes Down {
+    from {
+      opacity: 0%;
+    }
+    to {
+      opacity: 100%;
+    }
+  }
+`;
+
+export const EmailWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 6px;
+`;
+
+export const Email = styled.div`
+  position: relative;
+`;
+
+export const EamilButtonWrapper = styled.div`
+  position: absolute;
+  top: 7px;
+  right: 10px;
+  animation-name: fadeIn;
+  animation-duration: 0.6s;
+  animation-timing-function: ease-out;
+
+  @keyframes fadeIn {
+    from {
+      opacity: 30%;
+    }
+    to {
+      opacity: 100%;
+    }
+  }
+`;
+
+export const EmailCheckWrapper = styled.div`
+  position: relative;
+  animation-name: Down;
+  animation-duration: 0.6s;
+  animation-timing-function: ease-in;
+
+  @keyframes Down {
+    from {
+      opacity: 0%;
+    }
+    to {
+      opacity: 100%;
+    }
+  }
+`;
+
+export const PasswordWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  animation-name: Down;
+  animation-duration: 0.6s;
+  animation-timing-function: ease-in;
+
+  @keyframes Down {
+    from {
+      opacity: 0%;
+    }
+    to {
+      opacity: 100%;
+    }
+  }
+`;
+
+export const UsernameWrapper = styled.div`
+  animation-name: Down;
+  animation-duration: 0.6s;
+  animation-timing-function: ease-in;
+
+  @keyframes Down {
+    from {
+      opacity: 0%;
+    }
+    to {
+      opacity: 100%;
+    }
+  }
+`;
---
+++ src/components/Signup/SignupForm.tsx
@@ -0,0 +1,323 @@
+import { useEffect, useMemo, useState } from 'react';
+
+import * as Style from './SignupForm.style';
+
+import {
+  type InputNameProps,
+  type ButtonProps,
+  type InputProps,
+  type InputValidationProps
+} from '@/types';
+
+import {
+  emailValidation,
+  passworCheckValidation,
+  passwordValidation,
+  usernameValidation,
+  submitValidation
+} from '@/helpers/signup.helper';
+
+import Typography from '@/components/Common/Typography/Typography';
+import HeadTitle from '@/components/Common/Heading/HeadTitle';
+import Input from '@/components/Common/Input/Input';
+import Button from '@/components/Common/Button/Button';
+
+interface SignupInputProps {
+  email: {
+    value: string;
+    validation: InputValidationProps;
+    message: string;
+  };
+  emailCheck: {
+    value: string;
+    validation: InputValidationProps;
+    message: string;
+  };
+  password: {
+    value: string;
+    validation: InputValidationProps;
+    message: string;
+  };
+  passwordCheck: {
+    value: string;
+    validation: InputValidationProps;
+    message: string;
+  };
+  username: {
+    value: string;
+    validation: InputValidationProps;
+    message: string;
+  };
+}
+
+export default function SignupForm() {
+  const [userInput, setUserInput] = useState<SignupInputProps>({
+    email: {
+      value: '',
+      validation: 'default',
+      message: ''
+    },
+    emailCheck: {
+      value: '',
+      validation: 'default',
+      message: ''
+    },
+    password: {
+      value: '',
+      validation: 'default',
+      message: ''
+    },
+    passwordCheck: {
+      value: '',
+      validation: 'default',
+      message: ''
+    },
+    username: {
+      value: '',
+      validation: 'default',
+      message: ''
+    }
+  });
+
+  const [isAuthButtonClicked, setIsAuthButtonClicked] = useState(false);
+
+  useEffect(() => {
+    if (userInput.email.validation !== 'success') {
+      clearEmailCheckValue();
+    }
+  }, [userInput.email.validation]);
+
+  const isEmailCheckVisible = useMemo(
+    () => isAuthButtonClicked && userInput.email.validation === 'success',
+    [isAuthButtonClicked, userInput.email.validation]
+  );
+
+  const isValidateSubmit = useMemo(
+    () =>
+      submitValidation(
+        userInput.email.validation,
+        userInput.password.validation,
+        userInput.passwordCheck.validation,
+        userInput.username.validation
+      ),
+    [
+      userInput.email.validation,
+      userInput.password.validation,
+      userInput.passwordCheck.validation,
+      userInput.username.validation
+    ]
+  );
+
+  const clearEmailCheckValue = () => {
+    setIsAuthButtonClicked(false);
+    setUserInput((currentState) => ({
+      ...currentState,
+      emailCheck: {
+        value: '',
+        validation: 'default',
+        message: ''
+      }
+    }));
+  };
+
+  const handleEmailAuth = () => {
+    setUserInput((currentState) => ({
+      ...currentState,
+      emailCheck: {
+        ...currentState.emailCheck,
+        validation: 'success',
+        message: ''
+      }
+    }));
+  };
+
+  const handleUserInput = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const {
+      target: { name, value }
+    } = event;
+
+    setUserInput((currentState) => ({
+      ...currentState,
+      [name]: {
+        ...handleValidation(name as InputNameProps, value)
+      }
+    }));
+  };
+
+  const handleValidation = (name: InputNameProps, value: string) => {
+    switch (name) {
+      case 'email':
+        return emailValidation(value);
+      case 'password':
+        return passwordValidation(value);
+      case 'passwordCheck':
+        return passworCheckValidation(userInput.password.value, value);
+      case 'username':
+        return usernameValidation(value);
+      default:
+        return { value, validation: 'default', message: '' };
+    }
+  };
+
+  const handleEmailCheck = () => {
+    setIsAuthButtonClicked(true);
+  };
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+  };
+
+  const emailProps: InputProps = {
+    type: 'email',
+    name: 'email',
+    label: '이메일',
+    isLabelVisible: true,
+    isRequired: true,
+    size: 'lg',
+    value: userInput.email.value,
+    validation: userInput.email.validation,
+    message: userInput.email.message,
+    placeholder: '이메일을 입력해주세요.',
+    onChange: handleUserInput
+  };
+
+  const passwordProps: InputProps = {
+    type: 'password',
+    name: 'password',
+    label: '비밀번호',
+    isLabelVisible: true,
+    isRequired: true,
+    size: 'lg',
+    value: userInput.password.value,
+    validation: userInput.password.validation,
+    message: userInput.password.message,
+    placeholder: '비밀번호를 입력해주세요.',
+    onChange: handleUserInput
+  };
+
+  const passwordCheckProps: InputProps = {
+    type: 'password',
+    name: 'passwordCheck',
+    label: '비밀번호 확인',
+    isLabelVisible: true,
+    isRequired: true,
+    size: 'lg',
+    value: userInput.passwordCheck.value,
+    validation: userInput.passwordCheck.validation,
+    message: userInput.passwordCheck.message,
+    placeholder: '비밀번호를 다시 입력해주세요.',
+    onChange: handleUserInput
+  };
+
+  const usernameProps: InputProps = {
+    type: 'text',
+    name: 'username',
+    label: '닉네임',
+    isLabelVisible: true,
+    isRequired: true,
+    size: 'lg',
+    value: userInput.username.value,
+    validation: userInput.username.validation,
+    message: userInput.username.message,
+    placeholder: '닉네임을 입력해주세요.',
+    onChange: handleUserInput
+  };
+
+  const signupButtonProps: ButtonProps = {
+    type: 'submit',
+    size: 'lg',
+    name: 'signup',
+    disabled: isValidateSubmit,
+    children: (
+      <Typography size='button1' variant='accent'>
+        회원가입
+      </Typography>
+    )
+  };
+
+  const emailCheckProps: InputProps = {
+    type: 'text',
+    name: 'emailCheck',
+    label: '이메일 확인',
+    isLabelVisible: false,
+    size: 'lg',
+    value: userInput.emailCheck.value,
+    validation: userInput.emailCheck.validation,
+    message: userInput.emailCheck.message,
+    placeholder: '인증번호를 입력해주세요.',
+    onChange: handleUserInput
+  };
+
+  const emailCheckButtonProps: ButtonProps = {
+    type: 'button',
+    size: 'xsm',
+    name: 'emailCheck',
+    onClick: handleEmailCheck,
+    children: (
+      <Typography size='button2' variant='accent'>
+        인증하기
+      </Typography>
+    )
+  };
+
+  const emailAuthButtonProps: ButtonProps = {
+    type: 'button',
+    size: 'xsm',
+    name: 'emailCheck',
+    onClick: handleEmailAuth,
+    children: (
+      <Typography size='button2' variant='accent'>
+        확인
+      </Typography>
+    )
+  };
+
+  return (
+    <Style.SignupWrapper>
+      <Style.SignupHeader>
+        <HeadTitle>회원가입</HeadTitle>
+      </Style.SignupHeader>
+      <Style.SignupForm onSubmit={handleSubmit}>
+        <Style.InputWrapper>
+          <Style.EmailWrapper>
+            <Style.Email>
+              <Input {...emailProps} />
+              {userInput.email.validation === 'success' && (
+                <Style.EamilButtonWrapper>
+                  <Button {...emailCheckButtonProps} />
+                </Style.EamilButtonWrapper>
+              )}
+            </Style.Email>
+            {isEmailCheckVisible && (
+              <Style.EmailCheckWrapper>
+                <Input {...emailCheckProps} />
+                {userInput.emailCheck.value && (
+                  <Style.EamilButtonWrapper>
+                    <Button {...emailAuthButtonProps} />
+                  </Style.EamilButtonWrapper>
+                )}
+              </Style.EmailCheckWrapper>
+            )}
+          </Style.EmailWrapper>
+          {userInput.emailCheck.validation === 'success' && (
+            <Style.PasswordWrapper>
+              <Input {...passwordProps} />
+              <Input {...passwordCheckProps} />
+            </Style.PasswordWrapper>
+          )}
+          {userInput.password.validation === 'success' &&
+            userInput.passwordCheck.validation === 'success' && (
+              <Style.UsernameWrapper>
+                <Input {...usernameProps} />
+              </Style.UsernameWrapper>
+            )}
+        </Style.InputWrapper>
+        {!isValidateSubmit && (
+          <Style.SignupButtonWrapper>
+            <Button {...signupButtonProps} />
+          </Style.SignupButtonWrapper>
+        )}
+      </Style.SignupForm>
+    </Style.SignupWrapper>
+  );
+}
