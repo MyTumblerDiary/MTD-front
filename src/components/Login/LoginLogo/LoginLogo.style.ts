7421f074af861623344d016469bea241da4414ba
uuuuujin <yearn8546@gmail.com>
2023-04-04 22:15:54 +0900

    feat: 로고 컴포넌트 구현 (일단 텍스트로 대체)

---
+++ src/components/Login/LoginLogo/LoginLogo.style.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const LoginLogoContainer = styled.div`
+  width: 100%;
+  flex: 1;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
---
+++ src/components/Login/LoginLogo/LoginLogo.tsx
@@ -0,0 +1,13 @@
+import { HeadTitle } from '@/components/Common/Heading/Heading.style';
+
+import * as Style from './LoginLogo.style';
+
+const LoginLogo = () => {
+  return (
+    <Style.LoginLogoContainer>
+      <HeadTitle>로그인 로고</HeadTitle>;
+    </Style.LoginLogoContainer>
+  );
+};
+
+export default LoginLogo;
