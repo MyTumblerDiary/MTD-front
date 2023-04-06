95f76cb8efa28c671cd1542cb0b747ca5fc5afd5
task11 <task11@naver.com>
2023-04-06 19:57:43 +0900

    refactor: 유저 닉네임 유효성 정규식 수정

--- src/utils/constants/regExp.ts
+++ src/utils/constants/regExp.ts
@@ -10,7 +10,7 @@ export const PASSWORD_MIN_LENGTH = 8;
 
 export const PASSWORD_MAX_LENGTH = 15;
 
-export const NAME_MIN_LENGTH = 1;
+export const NAME_MIN_LENGTH = 2;
 
 export const NAME_MAX_LENGTH = 15;
 
