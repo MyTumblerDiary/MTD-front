02404d34575ac190da7ac193b71791ee3b264654
task11 <task11@naver.com>
2023-04-27 15:48:29 +0900

    feat: header 구현

--- src/components/Common/AuthHeader/AuthHeader.tsx
+++ src/components/Common/AuthHeader/AuthHeader.tsx
@@ -3,7 +3,6 @@ import * as Style from './AuthHeader.style';
 import Link from 'next/link';
 
 import { Title } from '@/components/Common/Heading/Heading.style';
-import Typography from '@/components/Common/Typography/Typography';
 import Svg from '@/components/svg';
 
 interface AuthHeaderProps {
