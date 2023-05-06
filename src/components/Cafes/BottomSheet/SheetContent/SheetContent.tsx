0ae023cefba4c1799eacc7f7b3e0b7edcdfad057
task11 <task11@naver.com>
2023-05-06 23:59:20 +0900

    feat: 카페 보기 페이지 바텀시트 콘텐츠 UI 구현

---
+++ src/components/Cafes/BottomSheet/SheetContent/SheetContent.tsx
@@ -0,0 +1,42 @@
+import * as Style from './SheetContent.style';
+
+import Typography from '@/components/Common/Typography/Typography';
+import Map from '../Map/Map';
+import Router from 'next/router';
+import { useToggleSheet } from '@/hooks';
+
+interface SheetContentProps {
+  discountAmount: number;
+  thumbnail: string;
+  latitude: number;
+  longitude: number;
+}
+
+export default function SheetContent({
+  discountAmount,
+  thumbnail,
+  latitude,
+  longitude
+}: SheetContentProps) {
+  const { toggleSheet } = useToggleSheet();
+
+  const linkToSuggestionPage = () => {
+    toggleSheet();
+    Router.push('/suggest-amount');
+  };
+
+  return (
+    <Style.SheetContent>
+      <Map markerImg={thumbnail} latitude={latitude} longitude={longitude} />
+      <Style.LinkToSuggestionPage onClick={linkToSuggestionPage}>
+        <Typography size='button1' variant='warning'>
+          할인 금액 수정 제안하기
+        </Typography>
+        <Style.VerticalLine />
+        <Typography size='button1' variant='warning'>
+          평균 {discountAmount}원
+        </Typography>
+      </Style.LinkToSuggestionPage>
+    </Style.SheetContent>
+  );
+}
--- src/components/Cafes/CafeList/CafeList.tsx
+++ src/components/Cafes/CafeList/CafeList.tsx
@@ -1,3 +1,48 @@
-export default function CafeList() {
-  return <div>CafeList</div>;
+import Typography from '@/components/Common/Typography/Typography';
+import * as Style from './CafeList.style';
+import { useToggleSheet } from '@/hooks';
+
+type CafesProps = CafeProps[];
+
+interface CafeProps {
+  id: number;
+  thumbnail: string;
+  cafe: string;
+  branch: string;
+  address: string;
+  discountAmount: number;
+}
+
+interface CafeListProps {
+  cafes: CafesProps;
+}
+
+export default function CafeList({ cafes }: CafeListProps) {
+  const { toggleSheet } = useToggleSheet();
+
+  return (
+    <Style.CafeListWrapper>
+      {cafes.map((item) => (
+        <Style.CafeCard key={item.id} onClick={toggleSheet}>
+          <Style.CafeSummary>
+            <Style.CafeThumbnail
+              src={item.thumbnail}
+              alt={`${item.cafe}'s thumbnail`}
+            />
+            <Style.CafeTitle>
+              <Typography size='button2'>
+                {item.cafe} {item.branch}
+              </Typography>
+              <Typography size='caption'>{item.address}</Typography>
+            </Style.CafeTitle>
+          </Style.CafeSummary>
+          <Style.CafeDiscountAmountWrapper>
+            <Typography size='button2' variant='main'>
+              {item.discountAmount}원 할인
+            </Typography>
+          </Style.CafeDiscountAmountWrapper>
+        </Style.CafeCard>
+      ))}
+    </Style.CafeListWrapper>
+  );
 }
