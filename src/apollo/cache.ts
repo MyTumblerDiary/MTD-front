2c489b96fc8994a177fc27a19f667ca7a1d8dff1
task11 <task11@naver.com>
2023-06-02 23:54:09 +0900

    feat: 페이지네이션을 위한 아폴로 클라이언트의 캐시 merge 기능 구현

--- src/apollo/cache.ts
+++ src/apollo/cache.ts
@@ -1,3 +1,19 @@
 import { InMemoryCache } from '@apollo/client';
 
-export const cache = new InMemoryCache();
+export const cache = new InMemoryCache({
+  typePolicies: {
+    Query: {
+      fields: {
+        stores: {
+          keyArgs: false,
+          merge(existing = [], incoming) {
+            if (!incoming.length) {
+              return existing;
+            }
+            return [...existing, ...incoming];
+          }
+        }
+      }
+    }
+  }
+});
