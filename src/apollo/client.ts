69df43af6143f63c6cab992d34c763d9f6d6fac4
uuuuujin <yearn8546@gmail.com>
2023-06-10 16:31:52 +0900

    chore: dev 브랜치와 merge하면서 생겼던 문제 해결

--- src/apollo/client.ts
+++ src/apollo/client.ts
@@ -16,16 +16,6 @@ const authLink = setContext((_, { headers }) => {
   };
 });
 
-const authLink = setContext((_, { headers }) => {
-  const token = localStorage.getItem('accessToken');
-  return {
-    headers: {
-      ...headers,
-      authorization: token ? `Bearer ${token}` : ''
-    }
-  };
-});
-
 export const client = new ApolloClient({
   link: authLink.concat(httpLink),
   cache
