b1fd3897a9e17bd5490ea9069942942e0bff2745
uuuuujin <yearn8546@gmail.com>
2023-06-10 18:33:08 +0900

    feat: 마이페이지 할인내역 페이지 api 연결 (무한스크롤 구현 필요)

--- src/components/Mypage/DiscountReport/DiscountReport.tsx
+++ src/components/Mypage/DiscountReport/DiscountReport.tsx
@@ -1,3 +1,5 @@
+import { useTumblerRecord } from '@/hooks';
+
 import Header from '@/components/Common/Header/Header';
 import DiscountInformation from '../DiscountInformation/DiscountInformation';
 import Typography from '@/components/Common/Typography/Typography';
@@ -5,34 +7,12 @@ import Typography from '@/components/Common/Typography/Typography';
 import * as Style from './DiscountReport.style';
 
 const DiscountReport = () => {
-  const DiscountInformationProps = {
-    count: 26,
-    amount: 5900
-  };
+  const { data } = useTumblerRecord({ order: 'ASC', searchBy: 'createdAt' });
 
-  const DUMMY_DATA = [
-    {
-      date: '05.06',
-      title: '투썸플레이스 인하대점',
-      memo: '메모메모',
-      price: 300,
-      amount: 5900
-    },
-    {
-      date: '05.06',
-      title: '투썸플레이스 인하대점',
-      memo: '메모메모',
-      price: 300,
-      amount: 5900
-    },
-    {
-      date: '05.06',
-      title: '투썸플레이스 인하대점',
-      memo: '메모메모',
-      price: 300,
-      amount: 5900
-    }
-  ];
+  const DiscountInformationProps = {
+    count: data?.tumblerRecords.totalUsedTumbler,
+    amount: data?.tumblerRecords.totalDiscount
+  };
 
   return (
     <div>
@@ -43,23 +23,25 @@ const DiscountReport = () => {
         </Style.DiscountInformationContainer>
 
         <Style.ListContainer>
-          {DUMMY_DATA.map((el, index) => {
-            const { date, title, memo, price, amount } = el;
+          {data?.tumblerRecords.tumblerRecords.map((el: any, index: number) => {
+            const { usedAt, placeType, memo, prices } = el;
             return (
-              <Style.ReportContainer key={`${date}-${index}`}>
-                <Typography size='body2'>{date}</Typography>
+              <Style.ReportContainer key={`${usedAt}-${index}`}>
+                <Typography size='body2'>
+                  {usedAt.split('-').splice(1).join('.')}
+                </Typography>
                 <Style.ReportTitleContainer>
-                  <Typography size='body1'>{title}</Typography>
+                  <Typography size='body1'>{placeType}</Typography>
                   <Typography size='body2' variant='gray3'>
                     {memo}
                   </Typography>
                 </Style.ReportTitleContainer>
                 <Style.ReportAmountContainer>
                   <Typography size='button1'>
-                    {price.toLocaleString()}원
+                    {prices?.toLocaleString() || 0}원
                   </Typography>
                   <Typography size='body2' variant='gray3'>
-                    {amount.toLocaleString()}원
+                    000원
                   </Typography>
                 </Style.ReportAmountContainer>
               </Style.ReportContainer>
